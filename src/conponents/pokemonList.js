a1f5e44765595c1602a6b803d90c303e9f94f61d
Jérémy Dejonghe <jeremy.dejonghe59@gmail.com>
2020-12-30 12:03:05 +0100

    Creation conponent statefull (pokemonList)

---
+++ src/conponents/pokemonList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class pokemonList extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
