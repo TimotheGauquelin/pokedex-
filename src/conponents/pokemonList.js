c553e1ca706c2150b5aad4eccaed63f1f6665ba0
Jérémy Dejonghe <jeremy.dejonghe59@gmail.com>
2020-12-30 12:08:55 +0100

    Initialisation conponents sur la branche develop

---
+++ src/conponents/pokemonList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class pokemonList extends Component {
+
+    
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
